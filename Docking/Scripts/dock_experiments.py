302683079b5756ac8dab92c74db96f662466aa22
vnica2 <116927189+vnica2@users.noreply.github.com>
2024-04-22 16:04:29 -0500

    Add files via upload

---
+++ Docking/Scripts/dock_experiments.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import subprocess
+import json
+import ast
+
+def extract_info_from_pdbqt(file_path):
+    smiles_scores = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith("REMARK VINA RESULT:"):
+                parts = line.split()
+                score = float(parts[3])
+                smiles = None
+                for sub_line in file:
+                    if sub_line.startswith("REMARK SMILES"):
+                        smiles = sub_line.split(maxsplit=1)[1].strip()
+                        break
+                if smiles:
+                    smiles_scores[smiles] = score
+    return smiles_scores
+
+
+def process_folder(folder_path):
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".txt"):
+            input_file_path = os.path.join(folder_path, file_name)
+            with open(input_file_path, 'r') as input_file:
+                contents = input_file.read()
+                smiles_set = ast.literal_eval(contents)
+            with open("temp_input.txt", "w") as temp_file:
+                for smile in smiles_set:
+                    temp_file.write(smile + "\n")
+            
+            subprocess.run(["python3", "helpers/ligprep.py", "temp_input.txt"])
+            subprocess.run(["python3", "helpers/vinawrapper.py"])
+            
+            smiles_scores = {}
+            for pdbqt_file in os.listdir("Ligands_out"):
+                if pdbqt_file.endswith(".pdbqt"):
+                    smiles_scores.update(extract_info_from_pdbqt(os.path.join("Ligands_out", pdbqt_file)))
+            
+            mean_score = sum(smiles_scores.values()) / len(smiles_scores)
+            median_score = sorted(smiles_scores.values())[len(smiles_scores) // 2]
+            std_dev = (sum((x - mean_score) ** 2 for x in smiles_scores.values()) / len(smiles_scores)) ** 0.5
+            max_score = min(smiles_scores.values())
+            
+            output_folder = os.path.join("final_experiment_molecules", "results")
+            os.makedirs(output_folder, exist_ok=True)
+            
+            output_file_name = f"{file_name.replace('.txt', '')}_mean_{mean_score:.2f}_median_{median_score:.2f}_std_{std_dev:.2f}_max_{max_score:.2f}.json"
+            output_file_path = os.path.join(output_folder, output_file_name)
+            with open(output_file_path, "w") as output_file:
+                json.dump(smiles_scores, output_file, indent=4)
+
+            # Clear contents of directories "Ligands" and "Ligands_out"
+            shutil.rmtree("../Ligands")
+            shutil.rmtree("../Ligands_out")
+            os.mkdir("../Ligands")
+            os.mkdir("../Ligands_out")
+
+# Get folders starting with "dpo_" or "sft_" inside the "final_experiment_results" folder
+experiment_folder = "final_experiment_molecules"
+folders_to_process = [os.path.join(experiment_folder, folder) for folder in os.listdir(experiment_folder) if folder.startswith("dpo_") or folder.startswith("sft_")]
+for folder in folders_to_process:
+    process_folder(folder)
---
+++ Docking/Scripts/helpers/ligprep.py
@@ -0,0 +1,107 @@
+import os
+import sys
+from meeko import MoleculePreparation, PDBQTWriterLegacy
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import shutil
+import concurrent.futures
+import subprocess
+
+def protonate_and_embed_with_obabel(smiles):
+    try:
+        # Run obabel command to protonate
+        obabel_command = f'obabel -:"{smiles}" -ismi -ocan -p7'
+        obabel_output = subprocess.check_output(obabel_command, shell=True, text=True).strip()
+
+        # Collect the results into a list
+        results = []
+        for state_idx, protonated_smiles in enumerate(obabel_output.split('\n')):
+            mol = Chem.MolFromSmiles(protonated_smiles)
+            AllChem.SanitizeMol(mol)
+            mol = Chem.AddHs(mol)
+            AllChem.EmbedMolecule(mol, randomSeed=42, useRandomCoords=True)
+            results.append((mol, state_idx))
+
+        return results
+
+    except Exception as e:
+        #print(f"Error processing SMILES: {smiles}")
+        #print(f"Error message: {e}")
+        #print(f"SMILES of failed molecule: {smiles}")
+        return []
+def save_to_pdbqt(mol_setup, directory, filename):
+    # Create the output directory if it doesn't exist
+    os.makedirs(directory, exist_ok=True)
+
+    # Combine directory and filename to get the full path
+    full_path = os.path.join(directory, filename)
+
+
+    # Generate PDBQT string using Meeko PDBQTWriterLegacy
+    pdbqt_string, _, _ = PDBQTWriterLegacy.write_string(mol_setup)
+
+    # Check if the PDBQT string is not empty#
+
+
+
+
+
+
+    if pdbqt_string.strip():
+        # Write the PDBQT string to a file
+        with open(full_path, 'w') as file:
+            file.write(pdbqt_string)
+        
+        #print(f'File {full_path} written successfully.')
+    else:
+        #print(f'Error: Empty PDBQT string for {filename}. Skipping.')
+        pass      
+
+def process_molecule(mol):
+    try:
+        # Prepare the molecule using Meeko
+        preparator = MoleculePreparation()
+        mol_setups = preparator.prepare(mol)
+
+        # Process each Meeko MoleculeSetup
+        for setup in mol_setups:
+            # Save the PDBQT string to a file with a unique filename for each protonation state
+            pdbqt_filename = f'molecule_{idx}_{state_idx}.pdbqt'
+            save_to_pdbqt(setup, default_output_directory, pdbqt_filename)
+
+    except Exception as e:
+        pass
+        #print(f"Error preparing molecule: {e}")
+        #print(f"Skipping molecule.")
+        
+if __name__ == "__main__":
+    # Check if the input file is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print("Usage: python ligprep.py input_file.txt")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    with open(input_file, "r") as file:
+        # Read SMILES strings from the file
+        smiles_list = [line.strip() for line in file]
+
+    # Specify the default output directory as a subdirectory named "PDBQT_Output"
+    default_output_directory = "../../Ligands"
+    if os.path.exists(default_output_directory):
+        shutil.rmtree(default_output_directory)
+    if os.path.exists("../../Ligands_out"):
+        shutil.rmtree("../../Ligands_out")
+
+    # Process each SMILES string using concurrent.futures
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for idx, results in enumerate(executor.map(protonate_and_embed_with_obabel, smiles_list)):
+            for mol, state_idx in results:
+                if mol is not None:
+                    AllChem.SanitizeMol(mol)
+                    process_molecule(mol)
+                
+                
+                
+
+
